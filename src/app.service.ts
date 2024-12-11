811dfec781f5fe34511334870d551c14ae732812
loraine19 <lou.hoffmann@gmail.com>
2024-12-11 13:30:02 +0100

    --

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,6 @@ export class AppService {
 
 
   getHello(): any{
-    return '<div class="container flex flex-col gap-4 px-8"><h1>First API</h1><a href="/api">Swagger</a><br><a href="/users">Users</a><br><a href="/groups">Groups</a><br><a href="https://pma.imagindev-app.fr/">PMA</a><br> <a href="https://primsa.imagindev-app.fr/">Prisma</a><div>';
+    return '<div class="container flex flex-col gap-4 px-8"><h1>First API</h1><a href="/api">Swagger</a><br><a href="/users">Users</a><br><a href="/groups">Groups</a><br><a href="/events">Events</a><br><a href="/address">Address</a><br><a href="https://pma.imagindev-app.fr/">PMA</a><br> <a href="https://primsa.imagindev-app.fr/">Prisma</a><div>';
   }
 }
