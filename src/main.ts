a155e725ae532a84edc1d1a1b1d31f6c40bba7ee
loraine19 <lou.hoffmann@gmail.com>
2024-12-11 01:17:07 +0100

    -cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,17 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors(
+    {
+      origin:
+        ['http://localhost:5173/',
+          'http://51.210.106.127:8080/',
+          'https://imagindev-app.fr/'
 
+        ]
+    }
+
+  );
 
   await app.listen(3000);
 }
