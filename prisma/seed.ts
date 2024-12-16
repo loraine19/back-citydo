cf3b4f0b2680ecd42b998626981b104d1da00231
loraine19 <lou.hoffmann@gmail.com>
2024-12-17 00:41:48 +0100

    --

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -136,7 +136,7 @@ const CreateRandomPost = (): CreatePostDto => {
 const CreateRandomLike = (): CreateLikeDto => {
   return {
     userId: newFaker.number.int({ min: 1, max }),
-    postId: newFaker.number.int({ min: 1, max })
+    postId: newFaker.helpers.uniqueArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)[0],
   }
 }
 
